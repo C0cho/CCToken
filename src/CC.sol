ce34438b549ac93c5ebe9dc910849550d7dd46cd
C0cho <693573510@qq.com>
2025-04-21 10:55:18 +0800

    CCtoken

--- src/CC.sol
+++ src/CC.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
-contract CC {
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+
+contract CC is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, OwnableUpgradeable{
     constructor(){
+        _disableInitializers();
 
     }
 }
