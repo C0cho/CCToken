2035d4fe06300f0db727cc5a614f5f7956b57c51
C0cho <693573510@qq.com>
2025-04-24 16:35:32 +0800

    Commit

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
-## Foundry
-
-**Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
-
-Foundry consists of:
-
--   **Forge**: Ethereum testing framework (like Truffle, Hardhat and DappTools).
--   **Cast**: Swiss army knife for interacting with EVM smart contracts, sending transactions and getting chain data.
--   **Anvil**: Local Ethereum node, akin to Ganache, Hardhat Network.
--   **Chisel**: Fast, utilitarian, and verbose solidity REPL.
+## 发行一个自己的代币 CC
 
 ## Documentation
 
@@ -21,24 +12,6 @@ https://book.getfoundry.sh/
 $ forge build
 ```
 
-### Test
-
-```shell
-$ forge test
-```
-
-### Format
-
-```shell
-$ forge fmt
-```
-
-### Gas Snapshots
-
-```shell
-$ forge snapshot
-```
-
 ### Anvil
 
 ```shell
@@ -48,13 +21,27 @@ $ anvil
 ### Deploy
 
 ```shell
-$ forge script script/Counter.s.sol:CounterScript --rpc-url <your_rpc_url> --private-key <your_private_key>
+$ forge script ./script/CC.s.sol:CCScript --rpc-url 127.0.0.1:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast
 ```
 
-### Cast
-
+### 查看代币发行的总量
 ```shell
-$ cast <subcommand>
+cast call --rpc-url 127.0.0.1:8545 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512 "totalSupply()(uint256)"
+```
+
+### 执行 mint
+```shell
+cast send --rpc-url 127.0.0.1:8545  --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512 "mint(address,uint256)" 0xee2E207D30383430a815390431298EBa3c1C8c2d 100000000
+```
+
+### 查看地址里面该代币数量
+```shell
+cast call --rpc-url 127.0.0.1:8545 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512 "balanceOf(address)(uint256)" 0xee2E207D30383430a815390431298EBa3c1C8c2d
+```
+
+### 转账
+```shell
+cast send --rpc-url 127.0.0.1:8545  --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512 "transfer(address,uint256)" 0xee2E207D30383430a815390431298EBa3c1C8c2d 100000000```
 ```
 
 ### Help
--- src/CC.sol
+++ src/CC.sol
@@ -1,15 +1,67 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 
 contract CC is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, OwnableUpgradeable{
+    string private constant NAME = "CC";
+    string private constant SYMBOL = "CC";
+    uint256 public constant MIN_MINT_INTERVAL = 365 days;
+    uint256 public constant MINT_CAP_DENOMINATOR = 10_1000;
+    uint256 public constant MINT_CAP_MAX_NUMERATOR = 200;
+    uint256 public mintCapNumerator;
+    uint256 public nextMint;
+
+    event MintCapNumeratorChanged(address indexed from, uint256 previousMintCapNumerator, uint256 newMintCapNumerator);
+
+    error CCToken_ImproperlyInitialized();
+    error CCToken_MintAmountTooLarge(uint256 amount, uint256 maximumAmount);
+    error CCToken_NextMintTimestampNotElapsed(uint256 currentTimestamp, uint256 nextMintTimestamp);
+    error CCToken_MintCapNumeratorTooLarge(uint256 numerator, uint256 maximumNumerator);
+
     constructor(){
         _disableInitializers();
-
     }
+
+    function initialize(uint256 _initialSupply, address _owner) public initializer {
+        if (_initialSupply == 0 || _owner == address(0)) revert CCToken_ImproperlyInitialized();
+
+        __ERC20_init(NAME, SYMBOL);
+        __ERC20Burnable_init();
+        __Ownable_init(_owner);
+        _mint(_owner, _initialSupply);
+        nextMint = block.timestamp + MIN_MINT_INTERVAL;
+        _transferOwnership(_owner);
+    }
+
+    function mint(address _recipient, uint256 _amount) public onlyOwner {
+        uint256 maximumMintAmount = (totalSupply() * mintCapNumerator) / MINT_CAP_DENOMINATOR;
+        if (_amount > maximumMintAmount) {
+            revert CCToken_MintAmountTooLarge(_amount, maximumMintAmount);
+        }
+        if (block.timestamp < nextMint) revert CCToken_NextMintTimestampNotElapsed(block.timestamp, nextMint);
+
+        nextMint = block.timestamp + MIN_MINT_INTERVAL;
+
+        super._mint(_recipient, _amount);
+    }
+
+    function setMintCapNumerator(uint256 _mintCapNumerator) public onlyOwner {
+        if (_mintCapNumerator > MINT_CAP_MAX_NUMERATOR) {
+            revert CCToken_MintCapNumeratorTooLarge(_mintCapNumerator, MINT_CAP_MAX_NUMERATOR);
+        }
+        emit MintCapNumeratorChanged(msg.sender,mintCapNumerator,_mintCapNumerator);
+        mintCapNumerator = _mintCapNumerator;
+    }
+
 }
+
+
+
+
+
+
